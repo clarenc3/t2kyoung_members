c8dc7e578dce6f61a37aa51f4f8d1cac24a9d2bd
Clarence <c.wret@rochester.edu>
2024-07-24 10:29:57 +0200

    Read the t2kyoung.csv file by default

--- check_members.py
+++ check_members.py
@@ -5,21 +5,21 @@
 import pandas as pd
 import datetime
 
-df = pd.read_csv('t2kyoung_first.csv')
+df = pd.read_csv('t2kyoung.csv')
 # Convert the time to datetime
 df['Member since']=pd.to_datetime(df['Member since'], format='%Y-%m-%d')
 #df.info()
 
-df_postdoc = df[df['Position'] == 'Postdoc']
+#df_postdoc = df[df['Position'] == 'Postdoc']
 #print(df_postdoc)
 
-df_phd = df[df['Position'] == 'Student PhD']
+#df_phd = df[df['Position'] == 'Student PhD']
 #print(df_phd)
 
-df_msc = df[df['Position'] == 'Student MSc']
+#df_msc = df[df['Position'] == 'Student MSc']
 #print(df_msc)
 
-mask=(df['Member since'] > '2022-1-1') & (df['Member since'] <= '2025-1-1')
+mask=(df['Member since'] > '2010-1-1') & (df['Member since'] <= '2018-1-1') & (df['Position'] == 'Postdoc')
 df = df.loc[mask]
 print(df)
 
